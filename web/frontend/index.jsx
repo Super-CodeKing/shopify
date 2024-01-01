54b52537580b2e999aa4ef661c5ef3080605ffac
Super-CodeKing <CharlieMorgan13542102321@outlook.com>
2024-01-01 10:09:59 +0600

    Setting up Redux

--- web/frontend/index.jsx
+++ web/frontend/index.jsx
@@ -1,11 +1,17 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import { initI18n } from "./utils/i18nUtils";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 const root = createRoot(container);
 
 initI18n().then(() => {
-  root.render(<App />);
+    root.render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
 });
